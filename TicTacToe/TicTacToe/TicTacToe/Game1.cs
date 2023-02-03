7945aa9ea1ca06b74ef346b487b0eef1ac358a42
EvanHolmes <squippysquerp@gmail.com>
2023-02-03 09:47:55 -0700

    BWawawawah

--- TicTacToe/TicTacToe/TicTacToe/Game1.cs
+++ TicTacToe/TicTacToe/TicTacToe/Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Diagnostics;
+using TicTacToe;
 
 namespace MONOGAMETicTacToeREREBOOT
 {
@@ -19,9 +20,7 @@ namespace MONOGAMETicTacToeREREBOOT
 
         float MouseX;
         float MouseY;
-        float DrawX;
-        float DrawY;
-        char Player = 'a';
+
         public enum GameState
         {
             Initialize,
@@ -31,6 +30,7 @@ namespace MONOGAMETicTacToeREREBOOT
             GameEnd
         }
         GameState currentGameState = GameState.Initialize;
+
         public enum MouseState
         {
             Released,
@@ -39,6 +39,7 @@ namespace MONOGAMETicTacToeREREBOOT
             WasPressedThisFrame,
         }
         MouseState currentMouseState = MouseState.Released;
+
         public enum Turn
         {
             OTurn,
@@ -52,8 +53,13 @@ namespace MONOGAMETicTacToeREREBOOT
             XState,
             OState
         }
+        RectangleState currentRectangleState = RectangleState.BlankState;
 
-        Rectangle[,] GameBoardArray = new Rectangle[3, 3];
+        Tile[,] GameBoardArray = new Tile[3, 3];
+
+    /// <summary>
+    /// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// </summary>
 
         public Game1()
         {
@@ -68,6 +74,8 @@ namespace MONOGAMETicTacToeREREBOOT
             _graphics.PreferredBackBufferHeight = BoardTexture.Height;
             _graphics.PreferredBackBufferWidth = BoardTexture.Width;
             _graphics.ApplyChanges();
+
+
 
             ExRectangle = ExTexture.Bounds;
             OhRectangle = OhTexture.Bounds;
@@ -85,7 +93,6 @@ namespace MONOGAMETicTacToeREREBOOT
 
         protected override void Update(GameTime gameTime)
         {
-
             switch (currentMouseState)
             {
                 case MouseState.Released:
@@ -93,8 +100,10 @@ namespace MONOGAMETicTacToeREREBOOT
                 case MouseState.Pressed:
                     break;
                 case MouseState.WasReleasedThisFrame:
+                    currentMouseState = MouseState.Released;
                     break;
                 case MouseState.WasPressedThisFrame:
+                    currentMouseState = MouseState.Pressed;
                     break;
                 default:
                     break;
@@ -105,20 +114,34 @@ namespace MONOGAMETicTacToeREREBOOT
                 case GameState.Initialize:
 
                     currentMouseState = MouseState.Released;
-                    for (int currentRow = 0; currentRow < 3; currentRow++)
-                    {
-                        for (int currentCol = 0; currentCol < 3; currentCol++)
-                        {
-                            GameBoardArray[currentRow, currentCol] =
-                                new Rectangle(new
-                                ((currentCol * 50) + (currentCol * 10),
-                                (currentRow * 50) + (currentRow * 10)),
-                                new(50, 50));
-                        }
-                    }
+                    //for (int currentRow = 0; currentRow < 3; currentRow++)
+                    //{
+                    //    for (int currentCol = 0; currentCol < 3; currentCol++)
+                    //    {
+                    //        GameBoardArray[currentRow, currentCol] =
+                    //            new Rectangle(new
+                    //            ((currentCol * 50) + (currentCol * 10),
+                    //            (currentRow * 50) + (currentRow * 10)),
+                    //            new(50, 50));
+                    //    }
+                    //}
 
+                    foreach (Tile tile in GameBoardArray)
+                    {
+                        tile.Reset();
+                    }
                     break;
                 case GameState.SwapTurn:
+                    if (currentTurn == Turn.OTurn)
+                    {
+                        currentTurn = Turn.XTurn;
+                    } else if (currentTurn == Turn.XTurn)
+                    {
+                        currentTurn = Turn.OTurn;
+                    } else
+                    {
+                        Debug.Write("SWAPTURN ERROR");
+                    }
                     break;
                 case GameState.ExecuteTurn:
                     break;
@@ -130,13 +153,12 @@ namespace MONOGAMETicTacToeREREBOOT
                     break;
             }
 
-
             if (Mouse.GetState().LeftButton == ButtonState.Pressed && DownLastFrame == false)
             {
+                MouseX = Mouse.GetState().X;
+                MouseY = Mouse.GetState().Y;
                 Debug.WriteLine(MouseX);
                 Debug.WriteLine(MouseY);
-                DrawX = MouseX - 25;
-                DrawY = MouseY - 25;
 
                 if (currentTurn == Turn.OTurn)
                 {
@@ -158,47 +180,33 @@ namespace MONOGAMETicTacToeREREBOOT
         }
 
 
-
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             // TODO: Add your drawing code here
-            switch (currentGameState)
-            {
-                case GameState.Initialize:
-                    break;
-                case GameState.SwapTurn:
-                    if (currentTurn == Turn.OTurn)
-                    {
-                        currentTurn = Turn.XTurn;
-                    }
-                    else
-                    {
-                        currentTurn = Turn.OTurn;
-                    }
-                    break;
-                case GameState.ExecuteTurn:
 
-                    break;
-                case GameState.EvaluateBoard:
-                    break;
-                case GameState.GameEnd:
-                    break;
-                default:
-                    break;
-            }
+
             _spriteBatch.Begin();
             _spriteBatch.Draw(BoardTexture, new Vector2(0, 0), Color.White);
 
-            foreach (Rectangle rectangle in GameBoardArray)
+            foreach (Tile tile in GameBoardArray)
             {
-                _spriteBatch.Draw(ExTexture, rectangle, Color.White);
+                if(tile._tileState != Tile.TileState.Blank)
+                {
+                    if (tile._tileState == Tile.TileState.X)
+                    {
+                        _spriteBatch.Draw(ExTexture, tile._rectangle, Color.White);
+                    }
+                    else if (tile._tileState == Tile.TileState.O)
+                    {
+                        _spriteBatch.Draw(OhTexture, tile._rectangle, Color.White);
+                    }
+                }
+                
             }
-
+             
             _spriteBatch.End();
-
             base.Draw(gameTime);
         }
     }
 }
-
---
+++ TicTacToe/TicTacToe/TicTacToe/Tile.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MONOGAMETicTacToeREREBOOT.Game1;
+
+namespace TicTacToe
+{
+    public class Tile //Knows where it is, know if the mouse is on it, know what state its in. DOES NOT need to know game state, textures etc.
+    {
+        public Rectangle _rectangle { get; private set;}
+
+        public enum TileState
+        {
+            Blank,
+            X,
+            O
+        }
+        public TileState _tileState { get; private set; }
+
+        public Tile(Rectangle rectangle)
+        {
+            _rectangle = rectangle;
+            _tileState = TileState.Blank;
+        }
+
+        public Tile(Rectangle tileBounds, TileState tileState)
+        {
+            _rectangle = tileBounds;
+            _tileState = tileState;
+        }
+
+        public void Reset()
+        {
+            _tileState = TileState.Blank;
+        }
+
+        public bool TryGetState(Point point, TileState state) // an X and Y co-ordinate and the state we want the Tile to be. We will do the X or O logic in main
+        {
+            if (_tileState == TileState.Blank && _rectangle.Contains(point))
+            {
+                _tileState = state;
+                return true;
+            }
+            return false;
+        }
+    }
+}
